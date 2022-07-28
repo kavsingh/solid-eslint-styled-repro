ffadc3e8bd5bef12aeadf92f653318609c4e09e4
Kav Singh <flakisms@gmail.com>
2022-07-29 01:30:41 +0200

    non-theme prop

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,17 @@ import { theme } from "./theme";
 
 import type { Component } from "solid-js";
 
-const Container = styled.div`
+const Container = styled.div<{ opacity: number }>`
   padding: ${(props) => props.theme?.spacing[1]};
   color: ${(props) => props.theme?.colors.text[400]};
   font-family: ${(props) => props.theme?.fonts.body};
   background-color: ${(props) => props.theme?.colors.surface[0]};
+  opacity: ${(props) => props.opacity};
 `;
 
 const App: Component = () => (
   <ThemeProvider theme={theme}>
-    <Container>Hello</Container>
+    <Container opacity={0.4}>Hello</Container>
   </ThemeProvider>
 );
 
